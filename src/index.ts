94287ed96fb1074188bed583ca81747fd084b231
Brandon Garling <bjg96@nau.edu>
2018-12-10 18:01:15 -0800

    Initial commit, setup project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Client } from 'discord.js';
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+ 
+client.login(process.env.API_KEY);
