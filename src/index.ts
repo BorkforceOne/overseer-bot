1e6e80751267200698bb01236c08b5f1809035ea
Brandon Garling <bjg96@nau.edu>
2018-12-10 18:05:55 -0800

    Move some stuffs around

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ const client = new Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
- 
+
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
 });
- 
+
 client.login(process.env.API_KEY);
