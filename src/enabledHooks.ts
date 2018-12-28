71b28c4bddbc6272a075e9443140cdb088d557b5
Brandon Garling <bjg96@nau.edu>
2018-12-27 20:56:44 -0800

    Add foogle hook

--- src/enabledHooks.ts
+++ src/enabledHooks.ts
@@ -1,7 +1,9 @@
 import { BogoVote } from './hooks/bogovoto/bogo_vote';
 import { SampleHook } from './hooks/sample_hook';
+import { FoogleHook } from './hooks/foogle/foogle'
 
 export const enabledHooks = [
 	SampleHook,
-	BogoVote,
+  BogoVote,
+  FoogleHook,
 ];
---
+++ src/hooks/foogle/foogle.ts
@@ -0,0 +1,31 @@
+import { Hook } from "../../hook";
+
+const lmgtfyTemplate = "https://lmgtfy.com/?q=";
+const hotword = '@google';
+const responses = [
+  "Here's what I found",
+  "This is what I can find",
+];
+
+/** Fake Google that just LMGTFYs whatever you look up */
+/** This hook is not associated with Google in any way */
+export class FoogleHook extends Hook {
+  public init() {
+    const { client } = this;
+
+    client.on("message", (msg) => {
+      if (msg.content.indexOf(hotword) === 0) {
+        const toEncode = msg.content.slice(hotword.length).trim();
+        if (toEncode.length === 0)
+          return;
+        const encodedURI = encodeURIComponent(toEncode);
+        const response = responses[Math.floor(Math.random()*responses.length)];
+        msg.reply({
+          embed: {
+            description: `[${response}](${lmgtfyTemplate}${encodedURI})`,
+          },
+        });
+      }
+    });
+  }
+}
