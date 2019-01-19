2d0f4505d3273b14ea2b29151101d1b4595a0757
Gage <inquisitorpinkie@gmail.com>
2019-01-18 17:26:30 -0700

    random emojis

--- src/enabledHooks.ts
+++ src/enabledHooks.ts
@@ -1,9 +1,11 @@
 import { BogoVote } from './hooks/bogovoto/bogo_vote';
 import { SampleHook } from './hooks/sample_hook';
-import { FoogleHook } from './hooks/foogle/foogle'
+import { FoogleHook } from './hooks/foogle/foogle';
+import { Randomoji } from './hooks/randomoji/randomoji';
 
 export const enabledHooks = [
 	SampleHook,
-  BogoVote,
-  FoogleHook,
+	BogoVote,
+	FoogleHook,
+	Randomoji,
 ];
