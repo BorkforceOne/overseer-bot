4c770fff3f05ad232d79ec45a2c0c8f0a72bcba9
Brandon Garling <bjg96@nau.edu>
2020-04-14 19:28:58 -0700

    Fix path issue when using an absolute path for firebase secrets.

--- src/services/app/data_service.ts
+++ src/services/app/data_service.ts
@@ -1,6 +1,7 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from 'firebase-admin';
 import { config } from '../../utils/config';
+import * as path from 'path';
 
 type Firestore = firebase.firestore.Firestore;
 
@@ -12,7 +13,13 @@ export class DataService {
   }
 
   init() {
-    const serviceAccount = require(__dirname + '../../../../' + config.firebaseSecretsPath);
+    let finalConfigLocation = config.firebaseSecretsPath;
+
+    if (!path.isAbsolute(config.firebaseSecretsPath)) {
+      finalConfigLocation = path.resolve(process.cwd(), config.firebaseSecretsPath);
+    }
+
+    const serviceAccount = require(finalConfigLocation);
     firebase.initializeApp({
       credential: firebase.credential.cert(serviceAccount),
       databaseURL: "https://magic-overseer-bot.firebaseio.com"
