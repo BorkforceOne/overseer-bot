2b44dc16193559d6cb0c77b5d62c7989c11fe062
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-04-25 18:31:02 -0700

    feature: version bot prints commit message

--- src/hooks/version/version.ts
+++ src/hooks/version/version.ts
@@ -2,6 +2,8 @@ import { Client, RichEmbed } from 'discord.js';
 
 import { DiscordService } from '../../services/app/discord_service';
 import { Hook } from '../../utils/hook';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 export class VersionHook implements Hook {
   private readonly client: Client;
@@ -17,7 +19,7 @@ export class VersionHook implements Hook {
       console.log(`Running version (${process.env.npm_package_version} - ${process.env.SOURCE_COMMIT})`);
     });
 
-    client.on("message", (msg) => {
+    client.on("message", async (msg) => {
       if (msg.content === "/version") {
         const baseUrl = 'https://github.com/bjg96/overseer-bot/'; // TODO use a process env?
         const commitHash = process.env.SOURCE_COMMIT;
@@ -25,8 +27,12 @@ export class VersionHook implements Hook {
         if (!version)
           throw new Error('No version number found!');
         const url = baseUrl + (commitHash ? 'commit/' + commitHash : '');
+        const res = await axios.get(url);
+        const $ = cheerio.load(res.data);
+        const title = $("title").text();
 
         const resp = new RichEmbed()
+          .setTitle(title)
           .setAuthor(
             client.user.username,
             client.user.avatarURL,
